7624d0279c979f686880572824b50e10f4a5f346
kidkudzy <kidkudzy@gmail.com>
2021-11-22 22:24:54 +0200

    first commit

---
+++ app_config.py
@@ -0,0 +1,33 @@
+
+from os import environ
+# CLIENT_SECRET=environ.get('CLIENT_SECRET')
+# CLIENT_ID=environ.get('CLIENT_ID')
+CLIENT_SECRET="eHz7Q~71YEmK-TGrKTzAZ02opN2ohsE6ad9~e"
+CLIENT_ID="c8881966-1379-4a8e-8dbf-c3c74c926ab1"
+
+
+ # Placeholder - for use ONLY during testing.
+# In a production app, we recommend you use a more secure method of storing your secret,
+# like Azure Key Vault. Or, use an environment variable as described in Flask's documentation:
+# https://flask.palletsprojects.com/en/1.1.x/config/#configuring-from-environment-variables
+
+# print(CLIENT_ID)
+# # if not CLIENT_SECRET:
+# #     raise ValueError("Need to define CLIENT_SECRET environment variable")
+
+AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app
+# AUTHORITY = "https://login.microsoftonline.com/7aaba9d7-83df-4d38-a058-6f68f7ec24d2"
+
+REDIRECT_PATH = "/getAToken"  # Used for forming an absolute URL to your redirect URI.
+                              # The absolute URL must match the redirect URI you set
+                              # in the app's registration in the Azure portal.
+
+# You can find more Microsoft Graph API endpoints from Graph Explorer
+# https://developer.microsoft.com/en-us/graph/graph-explorer
+ENDPOINT = 'https://graph.microsoft.com/v1.0/users'  # This resource requires no admin consent
+
+# You can find the proper permission names from this document
+# https://docs.microsoft.com/en-us/graph/permissions-reference
+SCOPE = ["User.ReadBasic.All"]
+
+SESSION_TYPE = "filesystem"  # Specifies the token cache should be stored in server-side session
---
+++ settings.py
@@ -0,0 +1,3 @@
+from os import environ
+CLIENT_SECRET=environ.get('CLIENT_SECRET')
+CLIENT_ID=environ.get('CLIENT_ID')
